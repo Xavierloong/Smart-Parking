7cebfde5e82da5affafc0929ce47ab5816393d32
Xavier Loong <howkianloong@outlook.com>
2020-10-05 16:39:43 +0800

    update chap 1

--- chap 1.py
+++ chap 1.py
@@ -1,10 +1,12 @@
 import cv2
+##Read image
+img = cv2.imread("Resources/lady.jpg")
 
-# img = cv2.imread("Resources/lady.jpg")
-#
-# cv2.imshow("Output",img)
-# cv2.waitKey(0)
+cv2.imshow("Output",img)
+cv2.waitKey(0)
 
+
+##Read Video
 cap = cv2.VideoCapture(0)
 cap.set(3,640)
 cap.set(4,480)
@@ -15,3 +17,5 @@ while True:
     cv2.imshow("nature",img)
     if cv2.waitKey(1)& 0xFF ==  ord('q'):
         break
+
+
